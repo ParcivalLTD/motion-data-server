ab4c7ee8ad3d33071ea3b4b448729f7b64c32d85
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-11-12 15:16:55 +0100

    sd

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -7,19 +6,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  socket.on('joinRoom', (code) => {
+    socket.join(code);
+  });
+
   socket.on('motionData', (data) => {
-    console.log('Motion data received:', data);
-    // Process and handle motion data as needed
-    io.emit('motionData', data);
+    const { code } = data;
+    io.to(code).emit('motionData', data);
   });
 
   socket.on('disconnect', () => {
@@ -27,8 +23,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
