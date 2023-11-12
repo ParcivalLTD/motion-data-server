e7022591eb3a03938157cb4ecee74eade9ae32b3
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-11-12 14:08:48 +0100

    test

--- server.js
+++ server.js
@@ -6,13 +6,38 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Store connected clients
+const clients = new Set();
+
 wss.on('connection', (ws) => {
+    console.log('Client connected');
+    clients.add(ws);
+
     // Handle incoming motion data from the client
     ws.on('message', (data) => {
-        console.log('Received motion data:', data);
-        // Process and handle motion data as needed 
+        const motionData = JSON.parse(data);
+        console.log('Received motion data:', motionData);
+
+        // Broadcast the motion data to all connected clients
+        broadcastMotionData(motionData);
+    });
+
+    // Handle client disconnection
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        clients.delete(ws);
     });
 });
+
+// Broadcast motion data to all connected clients
+function broadcastMotionData(data) {
+    const serializedData = JSON.stringify(data);
+    for (const client of clients) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(serializedData);
+        }
+    }
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
