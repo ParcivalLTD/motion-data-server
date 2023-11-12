4a0f4e727e57458bedcb84db6cb01991904dc81a
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-11-12 14:41:05 +0100

    rs

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
-// netlify/functions/motion.js
+const express = require('express');
+const app = express();
 
-const motionData = {};
+// Serve HTML file
+app.use(express.static('public'));
 
-exports.handler = async function (event, context) {
-  if (event.httpMethod === "POST") {
-    const { acceleration, rotationRate } = JSON.parse(event.body);
-    motionData.acceleration = acceleration;
-    motionData.rotationRate = rotationRate;
-    console.log("Received motion data:", motionData);
-    return {
-      statusCode: 200,
-      body: "Motion data received successfully.",
+// Endpoint to get motion data
+app.get('/motion-data', (req, res) => {
+    // Mock motion data for demonstration purposes
+    const motionData = {
+        accelerometer: { x: 0.1, y: 0.2, z: 9.8 },
+        gyroscope: { alpha: 0.01, beta: 0.02, gamma: 0.03 },
     };
-  } else if (event.httpMethod === "GET") {
-    if (Object.keys(motionData).length !== 0) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(motionData),
-      };
-    } else {
-      return {
-        statusCode: 404,
-        body: "Motion data not available.",
-      };
-    }
-  } else {
-    return {
-      statusCode: 405,
-      body: "Method Not Allowed",
-    };
-  }
-};
+
+    res.json(motionData);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
