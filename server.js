249e38679d485633133d2b2ac389aef5137d269d
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-11-12 14:59:39 +0100

    asd

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
-io.on("connection", (socket) => {
-    console.log("A user connected");
+app.use(express.static('public'));
 
-    // Listen for motion data from the PC
-    socket.on("sendMotionData", (data) => {
-        // Broadcast the motion data to all connected clients
-        io.emit("motionData", data);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('motionData', (data) => {
+    console.log('Motion data received:', data);
+    // Process and handle motion data as needed
+    io.emit('motionData', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
